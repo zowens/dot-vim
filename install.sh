2fc82a475cbc4d663434a20e0d3c90a3a7cc4dae
Zack Owens <zowens2009@gmail.com>
2011-07-18 19:19:17 -0400

    adding simpler install process

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+git clone http://github.com/zowens/dot-vim.git ~/.vim
+
 # create a link from .vim/vimrc from repo to 
 # .vimrc that VIM expects (same for gvim if necessary)
 ln -s ~/.vim/vimrc ~/.vimrc
 
 # update submodules
+cd ~/.vim
 git submodule update --init
