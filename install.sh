b1ef23fa27d74a1bd2fed2357e9f2d9dc5ada420
Zack Owens <zowens2009@gmail.com>
2011-07-18 23:19:01 -0400

    adding git/ssh-related overrides in scripts, also moving .vim in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+GIT_SSL_NO_VERIFY=true
+
+if [ -d "~/.vim" ]; then
+  echo "Moving old vim files from ~/.vim to ~/vim-old"
+  mv ~/.vim ~/vim-old
+  rmdir ~/.vim
+fi
+
 git clone http://github.com/zowens/dot-vim.git ~/.vim
 
 # create a link from .vim/vimrc from repo to 
--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git pull origin master
+GIT_SSL_NO_VERIFY=true
+git pull 
 git submodule foreach git pull origin master
