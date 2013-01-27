c3c408577337081fa2d826dd0587b35b4d3100cc
Zack Owens <zowens2009@gmail.com>
2013-01-27 14:42:34 -0500

    adding push step to update scripts

--- update.ps1
+++ update.ps1
@@ -3,3 +3,4 @@ git submodule update --init
 git submodule foreach git pull origin master
 git add bundle/
 git commit -m "updating modules"
+git push origin master
--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ env GIT_SSL_NO_VERIFY=true git submodule update --init
 env GIT_SSL_NO_VERIFY=true git submodule foreach git pull origin master
 git add bundle/
 git commit -m "updating modules"
+git push origin master
