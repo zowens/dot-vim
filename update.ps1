cd0698b98350c11f18393aab58365dd68f7ae94e
Zack Owens <zowens2009@gmail.com>
2013-03-14 14:08:47 -0400

    removing commit step in update... too ridiculous

--- update.ps1
+++ update.ps1
@@ -2,5 +2,3 @@ git pull origin master
 git submodule update --init
 git submodule foreach git pull origin master
 git add bundle/
-git commit -m "updating modules"
-git push origin master
--- update.sh
+++ update.sh
@@ -3,5 +3,3 @@ git pull origin master
 env GIT_SSL_NO_VERIFY=true git submodule update --init
 env GIT_SSL_NO_VERIFY=true git submodule foreach git pull origin master
 git add bundle/
-git commit -m "updating modules"
-git push origin master
