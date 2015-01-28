25ee43ce7afb429f5602f67a1de65020d55e596d
Zack Owens <zowens2009@gmail.com>
2015-01-27 20:09:31 -0500

    updating submodules

--- update.sh
+++ update.sh
@@ -2,11 +2,4 @@
 git pull origin master
 env GIT_SSL_NO_VERIFY=true git submodule update --init
 env GIT_SSL_NO_VERIFY=true git submodule foreach git pull origin master
-
-# updates gocode
-go get -u github.com/nsf/gocode
-git -C ~/src/gocode pull origin master
-cp -r ~/src/gocode/vim/autoload bundle/gocode
-cp -r ~/src/gocode/vim/ftplugin bundle/gocode
-
 git add bundle/
