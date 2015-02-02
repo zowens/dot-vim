dfeac435743fae915b22cece0523e40f79b2e58b
Zack Owens <zowens2009@gmail.com>
2015-02-01 19:56:48 -0500

    Adding modules

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-git pull origin master
-env GIT_SSL_NO_VERIFY=true git submodule update --init
-env GIT_SSL_NO_VERIFY=true git submodule foreach git pull origin master
-git add bundle/
+
+while read repository; do
+    dir=`basename $repository | sed 's/.git//g'`
+    if [[ -d bundle/$dir ]] ; then
+        git subtree pull --squash --prefix bundle/$dir $repository master
+    else 
+        git subtree add --squash --prefix bundle/$dir $repository master
+    fi
+done < modules
