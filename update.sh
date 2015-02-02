8f28d34d147bfbb85644d4e881a818623a337a6a
Zack Owens <zowens2009@gmail.com>
2015-02-01 20:07:02 -0500

    Adding note about stashing changes, quick adding submodules

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! git diff-index --quiet HEAD --; then
+    echo "Stash changes before upgrading dependencies"
+    exit 1
+fi
+
 while read repository; do
     dir=`basename $repository | sed 's/.git//g'`
     if [[ -d bundle/$dir ]] ; then
@@ -7,4 +12,9 @@ while read repository; do
     else 
         git subtree add --squash --prefix bundle/$dir $repository master
     fi
+
+    if ! git diff-index --quiet HEAD --; then
+        git add .
+        git commit --amend --no-edit
+    fi   
 done < modules
