b7a83200c7fae976b8f1ef8375a1395c10117db3
Zack Owens <zowens2009@gmail.com>
2015-02-09 10:55:10 -0500

    Squashed 'bundle/racer/' changes from 93d868e..34542a8
    34542a8 Ignore failing (to-be-implemented) tests for now to make travis build happy
    7230578 remove mut iter warning
    bd45c76 Merge pull request #144 from tafia/EnvClean
    af75e60 Merge pull request #142 from nyx/patch-1
    ebce1e6 Change std::os to std::env
    09624f9 Merge pull request #130 from tafia/ReviewScope
    f5a42e4 syntax::parse::token::InternedString usage updates
    d52d8e9 Add black_boxes to benchs
    5ab4197 Review scopes.rs
    7432251 Add bench_sub_scopes
    968d86a Add mask_comment bench
    186d390 Merge pull request #136 from renato-zannon/old-path
    06cb473 s/path/old_path/
    
    git-subtree-dir: bundle/racer
    git-subtree-split: 34542a88801d11a7fc9d4074dd3427a27766f6df

--- src/racer/bench.rs
+++ src/racer/bench.rs
@@ -1,17 +1,17 @@
 extern crate test;
 
-use std::old_io::fs::PathExtensions;
-use std::os::getenv;
+use std::env::var_string;
 use std::old_io::File;
 use self::test::Bencher;
 use racer::codecleaner::code_chunks;
 use racer::codeiter::iter_stmts;
+use racer::scopes::{mask_comments, mask_sub_scopes};
 
 fn get_rust_file_str(path: &[&str]) -> String {
 
-    let mut src_path = match getenv("RUST_SRC_PATH") {
-        Some(env) => { Path::new(&env[]) },
-        None => panic!("Cannot find $RUST_SRC_PATH")
+    let mut src_path = match var_string("RUST_SRC_PATH") {
+        Ok(env) => { Path::new(&env[]) },
+        _ => panic!("Cannot find $RUST_SRC_PATH")
     };
     for &s in path.iter() { src_path.push(s); }
 
@@ -22,7 +22,7 @@ fn get_rust_file_str(path: &[&str]) -> String {
 fn bench_code_chunks(b: &mut Bencher) {
     let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
     b.iter(|| {
-        let chunks = code_chunks(src).collect::<Vec<_>>();
+        test::black_box(code_chunks(src).collect::<Vec<_>>());
     });
 }
 
@@ -30,7 +30,23 @@ fn bench_code_chunks(b: &mut Bencher) {
 fn bench_iter_stmts(b: &mut Bencher) {
     let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
     b.iter(|| {
-        let chunks = iter_stmts(src).collect::<Vec<_>>();
+        test::black_box(iter_stmts(src).collect::<Vec<_>>());
+    });
+}
+
+#[bench]
+fn bench_mask_comments(b: &mut Bencher) {
+    let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
+    b.iter(|| {
+        test::black_box(mask_comments(src));
+    });
+}
+
+#[bench]
+fn bench_mask_sub_scopes(b: &mut Bencher) {
+    let src = &get_rust_file_str(&["libcollections", "bit.rs"])[];
+    b.iter(|| {
+        test::black_box(mask_sub_scopes(src));
     });
 }
 
